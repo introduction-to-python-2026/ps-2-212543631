56c93bfbc2baaa05c2cb8448ab117499e5c4ac05
212543631 <adi.shefer.43@gmail.com>
2025-11-03 13:49:23 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -4,6 +4,7 @@ def find_max_number(num1, num2, num3):
         return num1
       else: return num3
     else: return num2
+
 def find_mean(num1, num2, num3):
     mean = (num1 + num2 + num3)/3
     return mean
